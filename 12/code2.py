d4f178f397f600bfc0143627b25c4ddec7f182a1
Charles Croz <charles.croz@protonmail.com>
2024-12-14 16:15:00 +0100

    D12

--- 12/code2.py
+++ 12/code2.py
@@ -1,6 +1,6 @@
 def read_data():
     world_map = []
-    with open("./data00", 'r') as fp:
+    with open("./data0", 'r') as fp:
         for line in fp.readlines():
             world_map.append(line.strip())
     return world_map
@@ -14,6 +14,16 @@ def mutate(n):
         return [int(s[:l//2]), int(s[l//2:])]
     return [n * 2024]
 
+def grow(x, y, ident, world, ids):
+    ids[x][y] = ident
+    if x > 0 and ids[x-1][y] == -1 and world[x][y] == world[x-1][y]:
+        grow(x-1, y, ident, world, ids)
+    if y > 0 and ids[x][y-1] == -1 and world[x][y] == world[x][y-1]:
+        grow(x, y-1, ident, world, ids)
+    if x < len(world) - 1 and ids[x+1][y] == -1 and world[x][y] == world[x+1][y]:
+        grow(x+1, y, ident, world, ids)
+    if y < len(world[0]) - 1 and ids[x][y+1] == -1 and world[x][y] == world[x][y+1]:
+        grow(x, y+1, ident, world, ids)
 
 world_map = read_data()
 id_map = [] 
@@ -23,13 +33,45 @@ for l in world_map:
 id = 0
 for i in range(len(world_map)):
     for j in range(len(world_map[0])):
-        if i > 0 and world_map[i][j] == world_map[i-1][j]:
-            id_map[i][j] = id_map[i-1][j]
-        elif j > 0 and world_map[i][j] == world_map[i][j-1]:
-            id_map[i][j] = id_map[i][j-1]
-        else:
-            id_map[i][j] = id
+        if id_map[i][j] == -1:
+            grow(i, j, id, world_map, id_map)
             id += 1
+
+def is_out(i, j, world):
+    return i < 0 or j < 0 or i > len(world) - 1 or j > len(world[0]) - 1
+
+def is_top_border(i, j, world, id):
+    if is_out(i, j, world) or world[i][j] != id:
+        return False
+    elif is_out(i-1, j, world):
+        return True
+    else:
+        return world[i-1][j] != id
+
+def is_bottom_border(i, j, world, id):
+    if is_out(i, j, world) or world[i][j] != id:
+        return False
+    elif is_out(i+1, j, world):
+        return True
+    else:
+        return world[i+1][j] != id
+
+def is_left_border(i, j, world, id):
+    if is_out(i, j, world) or world[i][j] != id:
+        return False
+    elif is_out(i, j-1, world):
+        return True
+    else:
+        return world[i][j-1] != id
+
+def is_right_border(i, j, world, id):
+    if is_out(i, j, world) or world[i][j] != id:
+        return False
+    elif is_out(i, j+1, world):
+        return True
+    else:
+        return world[i][j+1] != id
+
 
 parcels = {}
 for i in range(len(world_map)):
@@ -38,20 +80,20 @@ for i in range(len(world_map)):
         if id in parcels.keys():
             parcels[id][0] += 1
         else:
-            parcels[id] = [1, 0]
-        if i == 0 or id_map[i][j] != id_map[i-1][j]:
+            parcels[id] = [1, 0, world_map[i][j]]
+        if is_top_border(i, j, world_map, world_map[i][j]) and not is_top_border(i, j - 1, world_map, world_map[i][j]):
             parcels[id][1] += 1
-        if j == 0 or id_map[i][j] != id_map[i][j-1]:
+        if is_left_border(i, j, world_map, world_map[i][j]) and not is_left_border(i - 1, j, world_map, world_map[i][j]):
             parcels[id][1] += 1
-        if i == len(world_map) - 1 or id_map[i][j] != id_map[i+1][j]:
+        if is_bottom_border(i, j, world_map, world_map[i][j]) and not is_bottom_border(i, j - 1, world_map, world_map[i][j]):
             parcels[id][1] += 1
-        if j == len(world_map[0]) - 1 or id_map[i][j] != id_map[i][j+1]:
+        if is_right_border(i, j, world_map, world_map[i][j]) and not is_right_border(i - 1, j, world_map, world_map[i][j]):
             parcels[id][1] += 1
 
 
 r = 0
 for k in parcels.keys():
-    # print(f"{k} > {parcels[k]}")
+    print(f"{k} > {parcels[k]}")
     r += parcels[k][0] * parcels[k][1]
 
 print(f"{r=}")
